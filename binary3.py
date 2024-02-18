3b839202658429d834bafbfde85690d2b1325bec
Sebastián Andrés Uribe Ruiz <96507066+SebasUr@users.noreply.github.com>
2024-02-17 19:12:29 -0500

    Add files via upload

---
+++ binary3.py
@@ -0,0 +1,16 @@
+from automata.fa.dfa import DFA
+
+dfa = DFA(
+    states={'q0', 'q1', 'q2'},
+    input_symbols={'0', '1'},
+    transitions={
+        'q0': {'0': 'q0', '1': 'q1'},
+        'q1': {'0': 'q2', '1': 'q0'},
+        'q2': {'0': 'q1', '1': 'q2'}
+    },
+    initial_state='q0',
+    final_states={'q0'}
+)
+
+string = input("$ ")
+print(f"The DFA accepts the string '{string}'" if dfa.accepts_input(string) else f"The DFA rejects the string '{string}'")
